e3c382a136fd3c1bfa139e4b16d32e0deb3465eb
Hisashi Morita <hisashim@icloud.com>
2026-02-14 12:32:38 +0900

    Update comments in debian.mk on its usage

--- debian.mk
+++ debian.mk
@@ -1,10 +1,19 @@
-# usage: make debianpackage GBP_EXTRA_OPTS="--git-pbuilder --git-pbuilder-options='--use-network yes'"
+# Debian package build script using git-buildpackage (gbp) and cowbuilder
+#
+# Usage:
+#   sudo apt install git-buildpackage cowbuilder
+#   sudo /usr/sbin/cowbuilder --update
+#   make -f debian.mk debianpackage \
+#     GBP_EXTRA_OPTS="--git-pbuilder --git-pbuilder-options='--use-network yes'" \
+#     | tee /tmp/hyoki-debianpackage-`date --utc --iso-8601=minutes`.log
+
 GBP_EXTRA_OPTS =
 
 debianpackage:
 	gbp buildpackage -us -uc \
 	--git-upstream-tree=`git branch --show-current` \
-	--git-ignore-branch $(GBP_EXTRA_OPTS)
+	--git-ignore-branch \
+	$(GBP_EXTRA_OPTS)
 
 debianpackageclean:
 	dh_clean
